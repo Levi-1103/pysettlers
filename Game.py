b511da791d76d26813ad8573fb8c2e7fd2e68552
Levi <58707841+Levi-1103@users.noreply.github.com>
2023-03-20 03:41:01 +0000

    Start of game class

--- Game.py
+++ Game.py
@@ -1,30 +1,22 @@
-# print(len(hexNum))
-# print(len((hexRes)))
+from Board2 import *
+from Player import *
+import random
+class Game:
+    def __init__(self,playerNum = 3):
+        self.board = Grid(7)
+        self.board.defaultBoard()
+        self.players = []
+        for i in range(playerNum):
+            self.players.append(Player(i))
+        
+    def buildSettlement(self,player,point):
+        pass
 
-#generate 18 hexes and nums and add to list
-#add desert tile with roll 7  at end of list
-# shuffle list
-#populate map with list
+    def buildRoad():
+        pass
 
-#possibly change list to hashmap
-#iterate through dict of edges check if it has owner
-# if its owned by a player add to point total
+    def upgradeSettlement():
+        pass
 
-#main game class that will be created when game is started
-#store game data which later than can be saved and reloaded
-
-#correlate points grid to hex height
-#add default sprites marking empty spot
-#add code for changi sprite to different icon when clicked
-#store points in dict in fomrat (q,r,N/S)
-#if N add to coord if S minus??
-
-#use named tuple for coords in dict ??
-
-#function for checking the vertices of a hex
-
-#make functions for accessing verticees and edges of a  hex
-#fix json import or coord convert  somethings wrong with coords as they need to be inverted to get correct ones
-
-#turn based game logic
-
+    def rollDice():
+        return random.randint(2,12)
