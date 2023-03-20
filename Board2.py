62a5ab9ce8748e68c6f6652307ca1e32a3dd167d
Levi <58707841+Levi-1103@users.noreply.github.com>
2023-03-20 03:35:02 +0000

    New hex grid representation and board drawing using pygame

---
+++ Board2.py
@@ -0,0 +1,183 @@
+from collections import namedtuple
+from Tile import *
+
+Hex  =  namedtuple("Hex","q r")
+Vertex = namedtuple("Vertex", "q r s")
+Edge  = namedtuple("Edge", "q r s")
+
+class Grid:
+    def __init__(self,size):
+        self.tiles = {}
+        self.edges = {}
+        self.vertices = {}
+        size
+
+        for q in range(size):
+            for r in range(size):
+                self.tiles.update({Hex(q,r) : 'Water'})
+                self.edges.update({Edge(q,r, 'NE'): ''})
+                self.edges.update({Edge(q,r, 'NW'): ''})
+                self.edges.update({Edge(q,r, 'W'): ''})
+                self.vertices.update({Vertex(q,r, 'N'): ''})
+                self.vertices.update({Vertex(q,r, 'S'): ''})
+        
+    #Caveman solution       
+    def defaultBoard(self):
+        #1st Row
+        self.tiles.update({Hex(3,1) : Tile(TileResource.Ore,10)})
+        self.tiles.update({Hex(4,1) : Tile(TileResource.Wool,2)})
+        self.tiles.update({Hex(5,1) : Tile(TileResource.Lumber,9)})
+        #2nd Row
+        self.tiles.update({Hex(2,2) : Tile(TileResource.Grain,12)})
+        self.tiles.update({Hex(3,2) : Tile(TileResource.Brick,6)})
+        self.tiles.update({Hex(4,2) : Tile(TileResource.Wool,4)})
+        self.tiles.update({Hex(5,2) : Tile(TileResource.Brick,10)})
+
+        #3rd Row
+        self.tiles.update({Hex(1,3) : Tile(TileResource.Grain,9)})
+        self.tiles.update({Hex(2,3) : Tile(TileResource.Lumber,11)})
+        self.tiles.update({Hex(3,3) : Tile(TileResource.Desert,7)})
+        self.tiles.update({Hex(4,3) : Tile(TileResource.Lumber,3)})
+        self.tiles.update({Hex(5,3) : Tile(TileResource.Ore,8)})
+        #4th Row
+        self.tiles.update({Hex(1,4) : Tile(TileResource.Lumber,8)})
+        self.tiles.update({Hex(2,4) : Tile(TileResource.Ore,3)})
+        self.tiles.update({Hex(3,4) : Tile(TileResource.Grain,4)})
+        self.tiles.update({Hex(4,4) : Tile(TileResource.Wool,5)})
+        #5th Row
+        self.tiles.update({Hex(1,5) : Tile(TileResource.Brick,5)})
+        self.tiles.update({Hex(2,5) : Tile(TileResource.Grain,6)})
+        self.tiles.update({Hex(3,5) : Tile(TileResource.Wool,11)})
+
+    
+
+    
+def neighbours(tile):
+    q = tile.q
+    r = tile.r
+    return [
+        Hex(q, r + 1),
+        Hex(q + 1, r),
+        Hex(q + 1, r - 1),
+        Hex(q, r - 1),
+        Hex(q - 1, r),
+        Hex(q - 1, r + 1)
+    ]
+
+def borders(tile):
+    q = tile.q
+    r = tile.r 
+    return [
+        Edge(q, r, 'NE'),
+        Edge(q, r, 'NW'),
+        Edge(q, r, 'W'),
+        Edge(q-1, r+1, 'NE'),
+        Edge(q, r+1, 'NW'),
+        Edge(q+1, r, 'W')
+    ]
+
+def corners(tile):
+    q = tile.q
+    r = tile.r
+
+    return [
+        Vertex(q, r, 'N'),
+        Vertex(q, r-1, 'S'),
+        Vertex(q-1, r+1, 'N'),
+        Vertex(q, r, 'S'),
+        Vertex(q, r+1, 'N'),
+        Vertex(q+1, r-1, 'S')
+    ]
+
+def joins(edge):
+    (q,r,s) = edge
+
+    if s == 'NE':
+        return [
+            Hex(q + 1, r -1),
+            Hex(q , r)
+        ]
+    elif s == 'NW':
+        return [
+            Hex(q , r),
+            Hex(q , r - 1)
+        ]
+    elif s == 'W':
+        return [
+            Hex(q, r),
+            Hex(q - 1, r)
+        ]
+
+def endpoints(edge):
+    (q,r,s) = edge
+    if s == 'NE':
+        return [
+            Vertex(q + 1, r -1, 'S'),
+            Vertex(q , r, 'N')
+        ]
+    elif s == 'NW':
+        return [
+            Vertex(q , r, 'N'),
+            Vertex(q , r - 1, 'S')
+        ]
+    elif s == 'W':
+        return [
+            Hex(q, r - 1, 'S'),
+            Hex(q - 1, r + 1, 'N')
+        ]
+
+    
+def touches(vertex):
+    (q,r,s) = vertex
+    if s == 'N':
+        return [
+            Hex(q + 1, r - 1),
+            Hex(q, r),
+            Hex(q, r - 1)
+        ]
+    if s == 'S':
+        return [
+            Hex(q, r),
+            Hex(q, r + 1),
+            Hex(q - 1, r + 1)
+        ]
+
+    
+def protrudes(vertex):
+    (q,r,s) = vertex
+    if s == 'N':
+        return [
+            Edge(q, r, 'NE'),
+            Edge(q+1, r-1, 'W'),
+            Edge(q, r, 'NW')
+        ]
+    if s == 'S':
+        return [
+            Edge(q, r+1, 'NW'),
+            Edge(q-1, r+1, 'NE'),
+            Edge(q, r+1, 'W')
+        ]
+    
+def adjacent(vertex):
+    (q,r,s) = vertex
+    if s == 'N':
+        return [
+            Vertex(q+1, r-2, 'S'),
+            Vertex(q, r-1, 'S'),
+            Vertex(q+1, r-1, 'S')
+        ]
+    if s == 'S':
+        return [
+            Vertex(q-1, r+1, 'N'),
+            Vertex(q-1, r+2, 'N'),
+            Vertex(q, r+1, 'N')
+        ]
+
+
+# def printNeigbourValues(tile):
+#     for key in neighbours(tile):
+#         print(game.tiles.get(key))
+
+
+
+
---
+++ testOutput.py
@@ -0,0 +1,141 @@
+import pygame
+import pygame_gui
+from drawHex import*
+from Board2 import *
+from TileResource import*
+import math
+
+pygame.init()
+
+width = 1600
+height = 900
+
+pygame.display.set_caption('The Settlers')
+window_surface = pygame.display.set_mode((width, height))
+
+background = pygame.Surface((width, height))
+background.fill(pygame.Color('#9cd3db'))
+
+boardrect = pygame.Rect(100,100,800,600)
+
+desert = pygame.image.load("assets\Desert.png").convert_alpha() #120x140
+brick = pygame.image.load("assets\Brick.png").convert_alpha()
+grain = pygame.image.load("assets\Grain.png").convert_alpha()
+lumber = pygame.image.load("assets\Lumber.png").convert_alpha()
+ore = pygame.image.load("assets\Ore.png").convert_alpha()
+wool = pygame.image.load("assets\Wool.png").convert_alpha()
+water = pygame.image.load("assets\Hex.png").convert_alpha()
+
+
+
+
+def textureToVal(value):
+    match value:
+        case TileResource.Brick:
+            return brick
+        case TileResource.Desert:
+            return desert
+        case TileResource.Grain:
+            return grain
+        case TileResource.Lumber:
+            return lumber
+        case TileResource.Ore:
+            return ore
+        case TileResource.Wool:
+            return wool
+        
+
+clock = pygame.time.Clock()
+is_running = True
+
+board = Grid(7)
+
+board.defaultBoard()
+
+
+
+
+
+# for coord in board:
+#         if board.get(coord) != '':
+#             background.blit(textureToVal(board[coord].resource), hexToPixel(75,coord.q,coord.r, 50))
+#         else:
+#             background.blit(desert, hexToPixel(75,coord[1],coord[0], 50))
+
+
+skip = [
+    Hex(0,0),
+    Hex(0,1),
+    Hex(2,0),
+    Hex(1,0),
+    Hex(1,1),
+    Hex(0,2),
+
+    Hex(6,4),
+    Hex(6,5),
+    Hex(5,5),
+    Hex(6,6),
+    Hex(5,6),
+    Hex(4,6)
+
+
+        ]
+
+for coord in board.tiles:
+    if coord not in skip:
+        pass
+    
+    if board.tiles[coord] != 'Water':
+       background.blit(textureToVal(board.tiles[coord].resource),hexToPixel(75,coord.q,coord.r, 50))
+    elif coord not in skip and board.tiles[coord] == 'Water':
+        background.blit(water, hexToPixel(75,coord.q,coord.r, 50))
+
+for key in touches(Vertex(3,3,'N')):
+    print(board.tiles.get(key).resource)
+
+# background.blit(brick, hexToPixel(75,0,2, 50))
+
+# for coord in board.vertices:
+#     if coord.s == 'N':
+#         pygame.draw.circle(background,'#FF0000',vertToPixel(75,coord.q,coord.r,110, 45),15,1)
+#     if coord.s == 'S':
+#         pygame.draw.circle(background,'#FFFFFF',vertToPixel(75,coord.q,coord.r,110,190),15,1)
+
+# for coord in board.edges:
+#     if coord.s == 'W':
+#         pygame.draw.circle(background,'#FF0000',vertToPixel(75,coord.q,coord.r,-15, 0),10)
+#     if coord.s == 'NW':
+#         pygame.draw.circle(background,'#FFFFFF',vertToPixel(75,coord.q,coord.r,20,-50),10)
+#     if coord.s == 'NE':
+#         pygame.draw.circle(background,'#000000',vertToPixel(75,coord.q,coord.r,70,-50),10)
+
+
+
+
+while is_running:
+    time_delta = clock.tick(60)/1000.0
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            is_running = False
+    
+    
+    
+    for key in corners(Hex(3,3)):
+        if key.s == 'N':
+            pygame.draw.circle(background,'#FF0000',vertToPixel(75,key.q,key.r,110,45),15)
+        if key.s == 'S':
+            pygame.draw.circle(background,'#FFFFFF',vertToPixel(75,key.q,key.r,110,190),15)
+
+    
+
+
+    
+
+    window_surface.blit(background, (0, 0))
+ 
+
+    pygame.display.update()
+
+
+
+
